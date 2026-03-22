ec7063e4efaea6e69f21a8caea54ebe837c8e32c
yymin1022 <yymin1022@gmail.com>
2026-03-22 17:28:01 +0900

    Update Project : Setup firebase project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 android {
