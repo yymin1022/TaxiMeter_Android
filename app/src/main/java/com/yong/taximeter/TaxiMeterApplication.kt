28da14199a2ee6082627463c133115a2b24fbb0e
yymin1022 <yymin1022@gmail.com>
2026-03-25 00:04:26 +0900

    Update App Module : Applied Hilt

---
+++ app/src/main/java/com/yong/taximeter/TaxiMeterApplication.kt
@@ -0,0 +1,7 @@
+package com.yong.taximeter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TaxiMeterApplication: Application()
--- app/src/main/java/com/yong/taximeter/activity/MainActivity.kt
+++ app/src/main/java/com/yong/taximeter/activity/MainActivity.kt
@@ -12,10 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yong.taximeter.common.ui.theme.TaxiMeterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             TaxiMeterTheme {
