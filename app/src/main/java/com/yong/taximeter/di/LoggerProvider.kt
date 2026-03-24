6fa9df33c2bc964fc6342ebd6938c94eba127f60
yymin1022 <yymin1022@gmail.com>
2026-03-25 00:08:10 +0900

    Update App Module : Added AppLogger DI Provider

---
+++ app/src/main/java/com/yong/taximeter/di/LoggerProvider.kt
@@ -0,0 +1,24 @@
+package com.yong.taximeter.di
+
+import android.content.Context
+import com.yong.taximeter.core.common.AppLogger
+import com.yong.taximeter.core.logging.AppLoggerImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Hilt DI Provider for [AppLogger]
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object LoggerModule {
+    @Provides
+    @Singleton
+    fun provideAppLogger(
+        @ApplicationContext context: Context,
+    ): AppLogger = AppLoggerImpl(context)
+}
