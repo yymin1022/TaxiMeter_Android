00b25273d19ddde2fa58c96f9cc3d29f016f21a9
yymin1022 <yymin1022@gmail.com>
2026-03-27 15:45:34 +0900

    Update App Module : Defined App Color constants

--- app/src/main/java/com/yong/taximeter/common/ui/theme/Color.kt
+++ app/src/main/java/com/yong/taximeter/common/ui/theme/Color.kt
@@ -1,11 +1,27 @@
 package com.yong.taximeter.common.ui.theme
 
+// ui/theme/Color.kt
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// App UI Colors
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Blue80 = Color(0xFFD1E4FF)
+val Blue40 = Color(0xFF0061A4)
+val Grey10 = Color(0xFF1A1C1E)
+val Grey90 = Color(0xFFE2E2E6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Meter UI Colors
+val MeterBackgroundDark = Color(0xFF111111)
+val MeterBackgroundLight = Color(0xFFF0F0F0)
+val MeterTextPrimaryDark = Color(0xFFEEEEEE)
+val MeterTextPrimaryLight = Color(0xFF111111)
+val MeterTextSecondaryDark = Color(0xFFCCCCCC)
+val MeterTextSecondaryLight = Color(0xFF444444)
+
+// Meter Colors
+val MeterBlue = Color(0xFF87CEEB)
+val MeterGreen = Color(0xFF99EE90)
+val MeterMint = Color(0xFF61D0BB)
+val MeterRed = Color(0xFFDB7093)
+val MeterYellow = Color(0xFFFFED83)
