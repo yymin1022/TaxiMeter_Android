d8813fef04a781b5def0c30d895ac58c309869ce
yymin1022 <yymin1022@gmail.com>
2026-03-28 21:48:03 +0900

    Update MainActivity : Applied navigation host

--- app/src/main/java/com/yong/taximeter/activity/MainActivity.kt
+++ app/src/main/java/com/yong/taximeter/activity/MainActivity.kt
@@ -7,11 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.yong.taximeter.common.ui.theme.TaxiMeterTheme
+import com.yong.taximeter.navigation.TaxiMeterNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,29 +21,21 @@ class MainActivity: ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaxiMeterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                ) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    // TaxiMeter Nav Host
+                    TaxiMeterNavHost(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues = innerPadding),
+                        navController = navController,
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TaxiMeterTheme {
-        Greeting("Android")
     }
 }
