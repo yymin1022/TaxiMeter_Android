7a9e1a3ff3034c4a647ffdfd0b6ace9986f023e3
yymin1022 <yymin1022@gmail.com>
2026-03-28 14:19:55 +0900

    Update App Module : Defined Meter Theme color setup

--- app/src/main/java/com/yong/taximeter/common/ui/theme/Theme.kt
+++ app/src/main/java/com/yong/taximeter/common/ui/theme/Theme.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
@@ -29,11 +31,12 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun TaxiMeterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    // App UI Color Scheme
+    // - Depends on dynamic color system
+    val appColorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -43,12 +46,25 @@ fun TaxiMeterTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    // Meter Color Scheme
+    val meterColorScheme = getMeterColors(isDark = darkTheme)
+
+    // Default App Theme, provides meter color scheme
+    CompositionLocalProvider(
+        LocalMeterColors provides meterColorScheme
+    ) {
+        MaterialTheme(
+            colorScheme = appColorScheme,
+            typography = Typography,
+            content = content,
+        )
+    }
 }
+
+/**
+ * Meter Color Provider
+ */
+val LocalMeterColors = staticCompositionLocalOf { getMeterColors(isDark = false) }
 
 /**
  * Color presets for Meter UI
@@ -84,12 +100,22 @@ private fun getMeterColors(
 } else {
     MeterColors(
         buttonText = MeterButtonTextLight,
-        blue = MeterBlueDark,
-        green = MeterGreenDark,
+        blue = MeterBlueLight,
+        green = MeterGreenLight,
         mint = MeterMint,
-        red = MeterRedDark,
-        yellow = MeterYellowDark,
+        red = MeterRedLight,
+        yellow = MeterYellowLight,
         background = MeterBackgroundLight,
         onBackground = MeterTextPrimaryLight,
     )
+}
+
+/**
+ * Color instance for get each color easily
+ * - Usage example: `MeterTheme.colors.blue`
+ */
+object MeterTheme {
+    val colors: MeterColors
+        @Composable
+        get() = LocalMeterColors.current
 }
