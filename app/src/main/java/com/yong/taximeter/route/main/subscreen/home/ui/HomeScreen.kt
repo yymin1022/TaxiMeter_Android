6c50099e2caf83e41abe860b5030db55e3d29c02
yymin1022 <yymin1022@gmail.com>
2026-03-29 15:43:01 +0900

    Added Home Screen / ViewModel

---
+++ app/src/main/java/com/yong/taximeter/route/main/subscreen/home/ui/HomeScreen.kt
@@ -0,0 +1,23 @@
+package com.yong.taximeter.route.main.subscreen.home.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.yong.taximeter.route.main.subscreen.home.viewmodel.HomeViewModel
+
+/**
+ * Home Screen
+ */
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel(),
+) {
+    Box(
+        modifier = modifier,
+    ) {
+        Text("Home Screen")
+    }
+}
---
+++ app/src/main/java/com/yong/taximeter/route/main/subscreen/home/viewmodel/HomeUiState.kt
@@ -0,0 +1,6 @@
+package com.yong.taximeter.route.main.subscreen.home.viewmodel
+
+/**
+ * UI State for [HomeViewModel]
+ */
+data object HomeUiState
