5274de7d22bd14ceb3960e69728bd44163688b43
yymin1022 <yymin1022@gmail.com>
2026-03-29 15:44:25 +0900

    Added Setting Screen / ViewModel

---
+++ app/src/main/java/com/yong/taximeter/route/main/subscreen/setting/viewmodel/SettingViewModel.kt
@@ -0,0 +1,17 @@
+package com.yong.taximeter.route.main.subscreen.setting.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    // Inject Dependencies
+): ViewModel() {
+    // UI State
+    private val _uiState: MutableStateFlow<SettingUiState> = MutableStateFlow(SettingUiState)
+    val uiState: StateFlow<SettingUiState> = _uiState.asStateFlow()
+}
