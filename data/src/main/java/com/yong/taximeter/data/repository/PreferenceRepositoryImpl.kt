c73232980e506740aae55121f2f7f1627eb20f8b
yymin1022 <yymin1022@gmail.com>
2026-04-01 23:24:02 +0900

    Update Data Module : Implemented Preference Repository Logic with Android Context

--- data/src/main/java/com/yong/taximeter/data/repository/PreferenceRepositoryImpl.kt
+++ data/src/main/java/com/yong/taximeter/data/repository/PreferenceRepositoryImpl.kt
@@ -1,58 +1,70 @@
 package com.yong.taximeter.data.repository
 
+import android.content.Context
 import com.yong.taximeter.domain.repository.PreferenceRepository
+import javax.inject.Inject
+import androidx.core.content.edit
 
 /**
  * Preference Repository Implementation
  * - Get / Set each preference values
  * - Boolean, Float, Int, String types are supported
  */
-class PreferenceRepositoryImpl: PreferenceRepository {
+class PreferenceRepositoryImpl @Inject constructor(
+    // Inject Android Context
+    private val context: Context,
+): PreferenceRepository {
+    companion object {
+        // Default preference name
+        private const val DEFAULT_PREF_NAME = "default_pref"
+    }
+
+    // Shared Preferences Instance from Android Context
+    private val pref by lazy {
+        context.getSharedPreferences(DEFAULT_PREF_NAME, Context.MODE_PRIVATE)
+    }
+
     /**
      * Getter / Setter for Boolean value
      */
     override fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        // TODO: Implement Getter
-        return defaultValue
+        return pref.getBoolean(key, defaultValue)
     }
 
     override fun setBoolean(key: String, value: Boolean) {
-        // TODO: Implement Setter
+        pref.edit { putBoolean(key, value) }
     }
 
     /**
      * Getter / Setter for Float value
      */
     override fun getFloat(key: String, defaultValue: Float): Float {
-        // TODO: Implement Getter
-        return defaultValue
+        return pref.getFloat(key, defaultValue)
     }
 
     override fun setFloat(key: String, value: Float) {
-        // TODO: Implement Setter
+        pref.edit { putFloat(key, value) }
     }
 
     /**
      * Getter / Setter for Int value
      */
     override fun getInt(key: String, defaultValue: Int): Int {
-        // TODO: Implement Getter
-        return defaultValue
+        return pref.getInt(key, defaultValue)
     }
 
     override fun setInt(key: String, value: Int) {
-        // TODO: Implement Setter
+        pref.edit { putInt(key, value) }
     }
 
     /**
      * Getter / Setter for String value
      */
     override fun getString(key: String, defaultValue: String): String {
-        // TODO: Implement Getter
-        return defaultValue
+        return pref.getString(key, defaultValue) ?: defaultValue
     }
 
     override fun setString(key: String, value: String) {
-        // TODO: Implement Setter
+        pref.edit { putString(key, value) }
     }
 }
