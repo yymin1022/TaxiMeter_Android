212163bba5b3e766978434499d6a888f60327570
yymin1022 <yymin1022@gmail.com>
2026-04-01 23:28:20 +0900

    Update App Module : Declared PreferenceRepository Provider

--- app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
+++ app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
@@ -1,8 +1,14 @@
 package com.yong.taximeter.di
 
+import android.content.Context
+import com.yong.taximeter.data.repository.PreferenceRepositoryImpl
+import com.yong.taximeter.domain.repository.PreferenceRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 /**
  * Hilt DI Provider for Repositories of Domain module
@@ -10,5 +16,9 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryProvider {
-    // TODO: Implement each repository provide methods
+    @Provides
+    @Singleton
+    fun providePreferenceRepository(
+        @ApplicationContext context: Context,
+    ): PreferenceRepository = PreferenceRepositoryImpl(context)
 }
