2d433c703e901635fafbdba93e60455265de830a
yymin1022 <yymin1022@gmail.com>
2026-04-01 23:27:15 +0900

    Update App Module : Added Repository Provider object

---
+++ app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
@@ -0,0 +1,14 @@
+package com.yong.taximeter.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Hilt DI Provider for Repositories of Domain module
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryProvider {
+    // TODO: Implement each repository provide methods
+}
