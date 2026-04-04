6b5c9105889f04890c2fb33bc6747e1807ee072f
yymin1022 <yymin1022@gmail.com>
2026-04-04 14:18:53 +0900

    Update Domain Module : Defined CostRepository Interface

---
+++ domain/src/main/java/com/yong/taximeter/domain/repository/CostRepository.kt
@@ -0,0 +1,20 @@
+package com.yong.taximeter.domain.repository
+
+import com.yong.taximeter.domain.model.CostInfo
+
+/**
+ * Cost Repository Interface
+ * - Get cost info for each region
+ * - Update to latest cost info
+ */
+interface CostRepository {
+    // Get Local / Remote cost version
+    fun getLocalVersion(): String
+    fun getRemoteVersion(): String
+
+    // Update local cost info to latest version
+    fun updateToLatest(): Boolean
+
+    // Get cost info for specific region
+    fun getCostInfo(regionKey: String): CostInfo?
+}
