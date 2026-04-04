b970d464a91c5d0c8412e1601cb6a6af8fb63131
yymin1022 <yymin1022@gmail.com>
2026-04-04 14:51:40 +0900

    Update Data Module : Defined CostInfo Entity / DAO

---
+++ data/src/main/java/com/yong/taximeter/data/dao/CostInfoDao.kt
@@ -0,0 +1,32 @@
+package com.yong.taximeter.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.yong.taximeter.data.entity.CostInfoEntity
+
+/**
+ * Cost Version DAO
+ */
+@Dao
+interface CostInfoDao {
+    @Query("SELECT * FROM cost_info WHERE region = :regionKey")
+    fun getByRegion(regionKey: String): CostInfoEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(costs: List<CostInfoEntity>)
+
+    @Query("DELETE FROM cost_info WHERE isCustom = 0")
+    suspend fun deleteAllRemoteCost()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCustomCost(cost: CostInfoEntity)
+
+    @Transaction
+    suspend fun updateRemoteCost(costs: List<CostInfoEntity>) {
+        deleteAllRemoteCost()
+        insertAll(costs)
+    }
+}
