b646923326a33d7648f3d3307f49d418f77edbf8
yymin1022 <yymin1022@gmail.com>
2026-04-04 20:29:22 +0900

    Update Data Module : Implemented cost info update logic to CostRepository

--- data/src/main/java/com/yong/taximeter/data/repository/CostRepositoryImpl.kt
+++ data/src/main/java/com/yong/taximeter/data/repository/CostRepositoryImpl.kt
@@ -1,14 +1,19 @@
 package com.yong.taximeter.data.repository
 
+import com.yong.taximeter.data.dao.CostInfoDao
 import com.yong.taximeter.data.datasource.FirestoreDataSource
 import com.yong.taximeter.data.datasource.PreferenceDataSource
+import com.yong.taximeter.data.dto.CostInfoDto
 import com.yong.taximeter.data.dto.CostVersionDto
+import com.yong.taximeter.data.mapper.CostInfoMapper.toEntity
 import com.yong.taximeter.domain.model.CostInfo
 import com.yong.taximeter.domain.repository.CostRepository
 import javax.inject.Inject
 import kotlin.jvm.java
 
 class CostRepositoryImpl @Inject constructor(
+    // Inject Cost DB DAO
+    private val costDao: CostInfoDao,
     // Inject Firestore DataSource
     private val firestoreDataSource: FirestoreDataSource,
     // Inject Preference DataSource
@@ -16,6 +21,7 @@ class CostRepositoryImpl @Inject constructor(
 ): CostRepository {
     companion object {
         private const val FIRESTORE_COLLECTION_KEY_COST = "cost"
+        private const val FIRESTORE_DOCUMENT_KEY_INFO = "info"
         private const val FIRESTORE_DOCUMENT_KEY_VERSION = "version"
 
         private const val PREF_KEY_COST_VERSION = "PREF_KEY_COST_VERSION"
@@ -46,9 +52,34 @@ class CostRepositoryImpl @Inject constructor(
         )?.version ?: COST_VERSION_FALLBACK
     }
 
-    override fun updateToLatest(): Boolean {
-        // TODO: Implement logic
-        return false
+    /**
+     * Get remote cost version from Firestore
+     * - If null or error, return [COST_VERSION_FALLBACK]
+     *
+     * @return true if update is successful, false otherwise
+     */
+    override suspend fun updateToLatest(): Boolean {
+        val costListDto = firestoreDataSource.getDocumentObject(
+            collection = FIRESTORE_COLLECTION_KEY_COST,
+            document = FIRESTORE_DOCUMENT_KEY_INFO,
+            clazz = CostInfoDto::class.java,
+        )
+
+        // If cost info is unavailable, return false
+        if(costListDto?.data.isNullOrEmpty()) return false
+
+        // Convert to entity, and update DAO
+        val costEntityList = costListDto.data.map { it.toEntity() }
+        costDao.updateRemoteCost(costEntityList)
+
+        // Get remote version info
+        val remoteVersion = getRemoteVersion()
+
+        // Update local version info
+        preferenceDataSource.setString(PREF_KEY_COST_VERSION, remoteVersion)
+
+        // Update succeed. return true
+        return true
     }
 
     override fun getCostInfo(
