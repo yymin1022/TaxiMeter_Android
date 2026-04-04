ffb72d83ca9ed533e770f910b15086081174ddf7
yymin1022 <yymin1022@gmail.com>
2026-04-04 14:56:05 +0900

    Update Data Module : Added DTO -> Entity logic to CostInfo Mapper

--- data/src/main/java/com/yong/taximeter/data/mapper/CostInfoMapper.kt
+++ data/src/main/java/com/yong/taximeter/data/mapper/CostInfoMapper.kt
@@ -1,8 +1,29 @@
 package com.yong.taximeter.data.mapper
 
+import com.yong.taximeter.data.dto.CostInfoItem
+import com.yong.taximeter.data.entity.CostInfoEntity
+
 /**
  * Cost Info Mapper
  * - DTO -> Entity
  * - Entity -> Domain Model
  */
-object CostInfoMapper
+object CostInfoMapper {
+    /**
+     * Map DTO -> Entity
+     */
+    fun CostInfoItem.toEntity() = CostInfoEntity(
+        region = region,
+        costBase = data.costBase,
+        distBase = data.distBase,
+        costRunPer = data.costRunPer,
+        costTimePer = data.costTimePer,
+        extraRateCity = data.extraRateCity,
+        extraRateNight1 = data.extraRateNight1,
+        extraRateNight2 = data.extraRateNight2,
+        nightStartHour1 = data.nightStartHour1,
+        nightStartHour2 = data.nightStartHour2,
+        nightEndHour1 = data.nightEndHour1,
+        nightEndHour2 = data.nightEndHour2,
+    )
+}
