3b4d989d3476c3ef441fb0104d507feb4f143b9f
yymin1022 <yymin1022@gmail.com>
2026-04-04 20:44:01 +0900

    Update Data Module : Defined DB Info to TaxiMeter Database definition

--- data/src/main/java/com/yong/taximeter/data/database/TaxiMeterDatabase.kt
+++ data/src/main/java/com/yong/taximeter/data/database/TaxiMeterDatabase.kt
@@ -1,8 +1,17 @@
 package com.yong.taximeter.data.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.yong.taximeter.data.dao.CostInfoDao
+import com.yong.taximeter.data.entity.CostInfoEntity
 
 /**
  * TaxiMeter Database based on Room
  */
-abstract class TaxiMeterDatabase: RoomDatabase()
+@Database(
+    entities = [CostInfoEntity::class],
+    version = 1
+)
+abstract class TaxiMeterDatabase: RoomDatabase() {
+    abstract fun costInfoDao(): CostInfoDao
+}
