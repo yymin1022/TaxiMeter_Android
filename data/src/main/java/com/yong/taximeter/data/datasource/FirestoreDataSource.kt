70bc0449c5ad0bdfa8d3a676cc0f6365bcbd9763
yymin1022 <yymin1022@gmail.com>
2026-04-04 11:53:02 +0900

    Update Data Module : Added Firestore DataSource

---
+++ data/src/main/java/com/yong/taximeter/data/datasource/FirestoreDataSource.kt
@@ -0,0 +1,9 @@
+package com.yong.taximeter.data.datasource
+
+/**
+ * Firestore Data Source
+ * - Get document from firestore, and convert it as object
+ */
+class FirestoreDataSource {
+    // TODO: Implement
+}
