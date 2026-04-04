74b216550b455e0c30ee0eb50800e325ebd630ae
yymin1022 <yymin1022@gmail.com>
2026-04-04 14:53:52 +0900

    Update Data Module : Defined CostInfo DTO

---
+++ data/src/main/java/com/yong/taximeter/data/dto/CostInfoDto.kt
@@ -0,0 +1,36 @@
+package com.yong.taximeter.data.dto
+
+/**
+ * Cost Info DTO
+ * - Firebase Firestore -> DTO
+ */
+data class CostInfoDto(
+    val data: List<CostInfoItem> = emptyList(),
+)
+
+data class CostInfoItem(
+    // Region Key
+    val region: String = "",
+
+    // Cost Info Data
+    val data: CostInfoData = CostInfoData(),
+)
+
+data class CostInfoData(
+    // Base cost info
+    val costBase: Int = 0,
+    val distBase: Int = 0,
+
+    // Info for cost calculation
+    val costRunPer: Int = 0,
+    val costTimePer: Int = 0,
+
+    // Extra rate info
+    val extraRateCity: Int = 0,
+    val extraRateNight1: Int = 0,
+    val extraRateNight2: Int = 0,
+    val nightStartHour1: Int = 0,
+    val nightStartHour2: Int = 0,
+    val nightEndHour1: Int = 0,
+    val nightEndHour2: Int = 0
+)
