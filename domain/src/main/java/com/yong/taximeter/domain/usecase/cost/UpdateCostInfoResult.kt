173cf887f1f3169a37ef5809d7ef51358328ccd6
yymin1022 <yymin1022@gmail.com>
2026-04-06 23:47:35 +0900

    Update Domain Module : Defined SyncCostResult for UseCase

---
+++ domain/src/main/java/com/yong/taximeter/domain/usecase/cost/UpdateCostInfoResult.kt
@@ -0,0 +1,11 @@
+package com.yong.taximeter.domain.usecase.cost
+
+/**
+ * Update Cost Info Result
+ * - Result of [UpdateCostInfoUseCase] invoke
+ */
+enum class UpdateCostInfoResult {
+    UP_TO_DATE,
+    SUCCESS,
+    FAILURE,
+}
