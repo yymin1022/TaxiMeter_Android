33e4a5d16629350d17726e46f88a90c46e03804e
yymin1022 <yymin1022@gmail.com>
2026-04-11 02:15:05 +0900

    Update App Module : Added SettingRepository to DI Provider

--- app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
+++ app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
@@ -4,7 +4,9 @@ import com.yong.taximeter.data.dao.CostInfoDao
 import com.yong.taximeter.data.datasource.FirestoreDataSource
 import com.yong.taximeter.data.datasource.PreferenceDataSource
 import com.yong.taximeter.data.repository.CostRepositoryImpl
+import com.yong.taximeter.data.repository.SettingRepositoryImpl
 import com.yong.taximeter.domain.repository.CostRepository
+import com.yong.taximeter.domain.repository.SettingRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +29,16 @@ object RepositoryProvider {
         return CostRepositoryImpl(
             costDao,
             firestoreDataSource,
+            preferenceDataSource,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideSettingRepository(
+        preferenceDataSource: PreferenceDataSource,
+    ): SettingRepository {
+        return SettingRepositoryImpl(
             preferenceDataSource,
         )
     }
