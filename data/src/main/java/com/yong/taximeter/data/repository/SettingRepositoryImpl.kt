07a64cae190438818ec14d8d23a6a10d5f423ecc
yymin1022 <yymin1022@gmail.com>
2026-04-11 02:13:20 +0900

    Update Data Module : Implemented SettingRepository logic

--- data/src/main/java/com/yong/taximeter/data/repository/SettingRepositoryImpl.kt
+++ data/src/main/java/com/yong/taximeter/data/repository/SettingRepositoryImpl.kt
@@ -1,26 +1,38 @@
 package com.yong.taximeter.data.repository
 
+import com.yong.taximeter.data.datasource.PreferenceDataSource
 import com.yong.taximeter.domain.model.RegionSetting
 import com.yong.taximeter.domain.model.ThemeSetting
 import com.yong.taximeter.domain.repository.SettingRepository
 import javax.inject.Inject
 
 class SettingRepositoryImpl @Inject constructor(
-    // Inject dependencies
+    // Inject Preference DataSource
+    private val preferenceDataSource: PreferenceDataSource,
 ): SettingRepository {
-    override fun getCurrentRegion(): RegionSetting {
-        // TODO
+    companion object {
+        private const val PREF_KEY_SETTING_REGION = "PREF_KEY_SETTING_REGION"
+        private const val PREF_KEY_SETTING_THEME = "PREF_KEY_SETTING_THEME"
+
+        private val DEFAULT_REGION = RegionSetting.SEOUL
+        private val DEFAULT_THEME = ThemeSetting.HORSE
     }
 
-    override fun setRegion(value: RegionSetting): Boolean {
-        // TODO
+    override fun getCurrentRegion(): RegionSetting {
+        val regionKey = preferenceDataSource.getString(PREF_KEY_SETTING_REGION, DEFAULT_REGION.key)
+        return RegionSetting.entries.find { it.key == regionKey } ?: DEFAULT_REGION
+    }
+
+    override fun setRegion(value: RegionSetting) {
+        preferenceDataSource.setString(PREF_KEY_SETTING_REGION, value.key)
     }
 
     override fun getCurrentTheme(): ThemeSetting {
-        // TODO
+        val themeKey = preferenceDataSource.getString(PREF_KEY_SETTING_THEME, DEFAULT_THEME.key)
+        return ThemeSetting.entries.find { it.key == themeKey } ?: DEFAULT_THEME
     }
 
-    override fun setTheme(value: ThemeSetting): Boolean {
-        // TODO
+    override fun setTheme(value: ThemeSetting) {
+        preferenceDataSource.setString(PREF_KEY_SETTING_THEME, value.key)
     }
 }
