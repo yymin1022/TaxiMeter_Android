faf8baa61e91bd47ff5795fa47bee2f7d152e556
yymin1022 <yymin1022@gmail.com>
2026-04-22 23:07:39 +0900

    Update Domain Module : Defined BillingRepository Interface

--- domain/src/main/java/com/yong/taximeter/domain/repository/BillingRepository.kt
+++ domain/src/main/java/com/yong/taximeter/domain/repository/BillingRepository.kt
@@ -1,4 +1,20 @@
 package com.yong.taximeter.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+import com.yong.taximeter.domain.model.BillingProduct
+import com.yong.taximeter.domain.model.BillingPurchase
+
 interface BillingRepository {
+    // Connect / Disconnect Billing Service
+    fun connect(): Result<Unit>
+    fun disconnect()
+
+    // Observe for Billing
+    fun observePurchases(): Flow<Result<List<BillingPurchase>>>
+
+    // Manage Purchase
+    fun acknowledgePurchase(purchaseToken: String): Result<Unit>
+    fun consumePurchase(purchaseToken: String): Result<Unit>
+    fun queryExistingPurchases(): Result<List<BillingPurchase>>
+    fun queryProducts(productIds: List<String>): Result<List<BillingProduct>>
 }
