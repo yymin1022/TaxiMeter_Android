0183fad579589270dec06c6dcfc54ca33cf04a65
yymin1022 <yymin1022@gmail.com>
2026-05-01 12:09:48 +0900

    Update App Module : Defined Product SKU IDs

--- app/src/main/java/com/yong/taximeter/route/main/subscreen/store/viewmodel/StoreViewModel.kt
+++ app/src/main/java/com/yong/taximeter/route/main/subscreen/store/viewmodel/StoreViewModel.kt
@@ -11,6 +11,15 @@ import javax.inject.Inject
 class StoreViewModel @Inject constructor(
     // Inject Dependencies
 ): ViewModel() {
+    companion object {
+        // Product SKU IDs
+        private const val SKU_REMOVE_ADVERTISEMENT = "ad_remove"
+        private const val SKU_DONATE_1000 = "donation_1000"
+        private const val SKU_DONATE_5000 = "donation_5000"
+        private const val SKU_DONATE_10000 = "donation_10000"
+        private const val SKU_DONATE_50000 = "donation_50000"
+    }
+
     // UI State
     private val _uiState: MutableStateFlow<StoreUiState> = MutableStateFlow(StoreUiState())
     val uiState: StateFlow<StoreUiState> = _uiState.asStateFlow()
