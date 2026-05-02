1d86d252e007b03a207206af6827a0c143582887
yymin1022 <yymin1022@gmail.com>
2026-05-02 23:38:18 +0900

    Update App Module : Defined Billing mapper method to ViewModel

--- app/src/main/java/com/yong/taximeter/route/main/subscreen/store/viewmodel/StoreViewModel.kt
+++ app/src/main/java/com/yong/taximeter/route/main/subscreen/store/viewmodel/StoreViewModel.kt
@@ -1,6 +1,9 @@
 package com.yong.taximeter.route.main.subscreen.store.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.yong.taximeter.domain.model.BillingProduct
+import com.yong.taximeter.domain.repository.BillingRepository
+import com.yong.taximeter.route.main.subscreen.store.model.ProductItem
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -23,4 +26,16 @@ class StoreViewModel @Inject constructor(
     // UI State
     private val _uiState: MutableStateFlow<StoreUiState> = MutableStateFlow(StoreUiState())
     val uiState: StateFlow<StoreUiState> = _uiState.asStateFlow()
+
+    /**
+     * Map [BillingProduct] to [ProductItem]
+     */
+    private fun BillingProduct.asProductItem(
+        isPurchased: Boolean,
+    ) = ProductItem(
+        title = this.name,
+        desc = this.description,
+        formattedPrice = this.formattedPrice,
+        isPurchased = isPurchased,
+    )
 }
