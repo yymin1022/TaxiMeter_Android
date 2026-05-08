34ba7491c9c60cae16955ac1fa0cf82aed4c676e
yymin1022 <yymin1022@gmail.com>
2026-05-08 23:38:49 +0900

    Update Domain Module : Defined PreferenceDefs for Pref Keys definition

---
+++ domain/src/main/java/com/yong/taximeter/domain/defs/PreferenceDefs.kt
@@ -0,0 +1,13 @@
+package com.yong.taximeter.domain.defs
+
+/**
+ * Preference Definitions
+ */
+object PreferenceDefs {
+    const val PREF_KEY_AD_REMOVE = "PREF_KEY_AD_REMOVE"
+
+    const val PREF_KEY_COST_VERSION = "PREF_KEY_COST_VERSION"
+
+    const val PREF_KEY_SETTING_REGION = "PREF_KEY_SETTING_REGION"
+    const val PREF_KEY_SETTING_THEME = "PREF_KEY_SETTING_THEME"
+}
