b44562dd938d5e8aa471405db917f906d47e4675
yymin1022 <yymin1022@gmail.com>
2026-05-10 14:52:24 +0900

    Update MainActivity : Cleaned up

--- app/src/main/java/com/yong/taximeter/activity/MainActivity.kt
+++ app/src/main/java/com/yong/taximeter/activity/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.yong.taximeter.common.ui.theme.TaxiMeterTheme
