f3b364eb7e9c31fc4be5c51f53cc00dda33577cc
yymin1022 <yymin1022@gmail.com>
2026-05-16 20:27:29 +0900

    meter defs

--- domain/src/main/java/com/yong/taximeter/domain/defs/MeterDefs.kt
+++ domain/src/main/java/com/yong/taximeter/domain/defs/MeterDefs.kt
@@ -4,6 +4,12 @@ package com.yong.taximeter.domain.defs
  * Meter Definitions
  */
 object MeterDefs {
+    // Cost increasing unit
+    const val COST_UNIT = 100
+
     // Meter update interval (ms)
     const val METER_UPDATE_INTERVAL_MS = 500L
+
+    // Speed threshold with distance/time cost system (km/h)
+    const val SPEED_THRESHOLD_KPH = 15.0
 }
