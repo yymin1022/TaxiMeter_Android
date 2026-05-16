eb9b71046bddbb62a333035f4ac479635fb81697
yymin1022 <yymin1022@gmail.com>
2026-05-16 16:39:19 +0900

    Update App Module : Added LocationRepository to DI Provider

--- app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
+++ app/src/main/java/com/yong/taximeter/di/RepositoryProvider.kt
@@ -1,15 +1,21 @@
 package com.yong.taximeter.di
 
 import android.content.Context
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.yong.taximeter.core.common.AppLogger
 import com.yong.taximeter.data.dao.CostInfoDao
 import com.yong.taximeter.data.datasource.FirestoreDataSource
 import com.yong.taximeter.data.datasource.PreferenceDataSource
 import com.yong.taximeter.data.repository.BillingRepositoryImpl
 import com.yong.taximeter.data.repository.CostRepositoryImpl
+import com.yong.taximeter.data.repository.LocationRepositoryImpl
 import com.yong.taximeter.data.repository.SettingRepositoryImpl
+import com.yong.taximeter.domain.defs.MeterDefs
 import com.yong.taximeter.domain.repository.BillingRepository
 import com.yong.taximeter.domain.repository.CostRepository
+import com.yong.taximeter.domain.repository.LocationRepository
 import com.yong.taximeter.domain.repository.SettingRepository
 import dagger.Module
 import dagger.Provides
@@ -47,6 +53,23 @@ object RepositoryProvider {
             costDao,
             firestoreDataSource,
             preferenceDataSource,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocationRepository(
+        @ApplicationContext context: Context,
+    ): LocationRepository {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            MeterDefs.METER_UPDATE_MINIMUM_INTERVAL_MS,
+        ).build()
+
+        return LocationRepositoryImpl(
+            fusedLocationClient,
+            locationRequest,
         )
     }
 
