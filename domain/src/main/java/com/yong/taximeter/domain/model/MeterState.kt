555790c98ec40292cef24276cb6ebf43b03191b8
yymin1022 <yymin1022@gmail.com>
2026-05-16 16:45:51 +0900

    Update Domain Module : Defined MeterState data class

---
+++ domain/src/main/java/com/yong/taximeter/domain/model/MeterState.kt
@@ -0,0 +1,17 @@
+package com.yong.taximeter.domain.model
+
+/**
+ * Meter State Data Class
+ */
+data class MeterState(
+    // Current cost
+    val currentCost: Int = 0,
+    // Drove distance (m)
+    val totalDistanceMeters: Double = 0.0,
+    // Drove time (s)
+    val totalElapsedSeconds: Double = 0.0,
+    // Current speed (km/h)
+    val currentSpeedKph: Double = 0.0,
+    // Meter status
+    val status: MeterStatus = MeterStatus.NOT_RUNNING,
+)
