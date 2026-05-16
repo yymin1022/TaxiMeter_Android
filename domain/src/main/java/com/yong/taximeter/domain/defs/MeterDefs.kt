27e4ef15e061d418a9c92db7862b68fb766bc021
yymin1022 <yymin1022@gmail.com>
2026-05-16 19:43:28 +0900

    Meter Defs

--- domain/src/main/java/com/yong/taximeter/domain/defs/MeterDefs.kt
+++ domain/src/main/java/com/yong/taximeter/domain/defs/MeterDefs.kt
@@ -4,6 +4,6 @@ package com.yong.taximeter.domain.defs
  * Meter Definitions
  */
 object MeterDefs {
-    // Minimum update interval (ms)
-    const val METER_UPDATE_MINIMUM_INTERVAL_MS = 500L
+    // Meter update interval (ms)
+    const val METER_UPDATE_INTERVAL_MS = 500L
 }
