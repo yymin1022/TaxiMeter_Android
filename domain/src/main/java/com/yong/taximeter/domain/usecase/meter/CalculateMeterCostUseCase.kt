8110df6efe94f867972189812c5c4954bc4253f3
yymin1022 <yymin1022@gmail.com>
2026-05-17 16:07:48 +0900

    Update Domain Module : Adjust CalculateMeterCost UseCase parameter

--- domain/src/main/java/com/yong/taximeter/domain/usecase/meter/CalculateMeterCostUseCase.kt
+++ domain/src/main/java/com/yong/taximeter/domain/usecase/meter/CalculateMeterCostUseCase.kt
@@ -3,6 +3,7 @@ package com.yong.taximeter.domain.usecase.meter
 import com.yong.taximeter.domain.model.CostInfo
 import com.yong.taximeter.domain.model.MeterState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.scan
 import javax.inject.Inject
@@ -22,9 +23,12 @@ class CalculateMeterCostUseCase @Inject constructor(
      * @param costInfo Cost info for current region
      * @param isCityRate Whether to apply city surcharge
      */
-    operator fun invoke(costInfo: CostInfo, isCityRate: Boolean): Flow<MeterState> {
+    operator fun invoke(costInfo: CostInfo, isCityRate: Flow<Boolean>, ): Flow<MeterState> {
         return observeSpeedUseCase()
-            .scan(MeterCostCalculator.newWithCostInfo(costInfo)) { acc, speedData ->
+            .combine(isCityRate) { speedData, isCityRate ->
+                speedData to isCityRate
+            }
+            .scan(MeterCostCalculator.newWithCostInfo(costInfo)) { acc, (speedData, isCityRate) ->
                 acc.update(speedData, isCityRate)
             }
             .map { it.toMeterState() }
